6614fcb1b5d0bbabac7c47660a077382710f2b00
AirBird92 <albert19920831@gmail.com>
2026-08-05 17:55:50 -0700

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        board = [[''] * n for _ in range(n)]
+        def placeQueen(x, y, board):
+            for i in range(n):
+                for j in range(n):
+                    if i == x or j == y or abs(i - x) == abs(j - y):
+                        board[i][j] = '.'
+            board[x][y] = 'Q'
+        def dfs(i, board):
+            if i >= n:
+                res.append(["".join(x) for x in board])
+                return
+            for j in range(n):
+                if board[i][j] == '.':
+                    continue
+                curBoard = [row.copy() for row in board]
+                placeQueen(i, j, board)
+                dfs(i + 1, board)
+                board = curBoard
+        dfs(0, board)
+        return res
