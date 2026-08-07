8eeb88792ba51eae4acba592b4d4fba83d70fe42
AirBird92 <albert19920831@gmail.com>
2026-08-07 12:57:29 -0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        mem = [1, 1]
+        for _ in range(1, n):
+            s = mem[0] + mem[1]
+            mem[0] = mem[1]
+            mem[1] = s
+            print(mem)
+        return mem[1]
